e428b30dc3640d38a3923998d6be119cdcb610fb
rockgold0911 <rockgold0911@gmail.com>
2022-12-06 11:28:36 +0800

    feat(lib): protocol add commons

---
+++ lib/protocol/typechain/commons/BaseService.ts
@@ -0,0 +1,42 @@
+import type { Provider } from '@ethersproject/providers'
+import type { Contract, PopulatedTransaction, providers, Signer } from 'ethers'
+import type { tEthereumAddress, TransactionGenerationMethod, transactionType } from './types'
+import { DEFAULT_NULL_VALUE_ON_TX } from './utils'
+
+export interface ContractsFactory {
+  connect: (address: string, signerOrProvider: Signer | Provider) => Contract
+}
+
+export default class BaseService<T extends Contract> {
+  readonly contractInstances: Record<string, T>
+
+  readonly contractFactory: ContractsFactory
+
+  readonly provider: providers.Provider
+
+  constructor(provider: providers.Provider, contractFactory: ContractsFactory) {
+    this.contractFactory = contractFactory
+    this.provider = provider
+    this.contractInstances = {}
+  }
+
+  public getContractInstance = (address: tEthereumAddress): T => {
+    if (!this.contractInstances[address]) {
+      this.contractInstances[address] = this.contractFactory.connect(address, this.provider) as T
+    }
+
+    return this.contractInstances[address]
+  }
+
+  readonly generateTxCallback =
+    ({ rawTxMethod, from, value }: TransactionGenerationMethod): (() => Promise<transactionType>) =>
+    async () => {
+      const txRaw: PopulatedTransaction = await rawTxMethod()
+      const tx: transactionType = {
+        ...txRaw,
+        from,
+        value: value ?? DEFAULT_NULL_VALUE_ON_TX,
+      }
+      return tx
+    }
+}
---
+++ lib/protocol/typechain/commons/types.ts
@@ -0,0 +1,21 @@
+import type { BigNumber, PopulatedTransaction } from 'ethers'
+
+export type tEthereumAddress = string
+export type ENS = string // something.eth
+
+export type TransactionGenerationMethod = {
+  rawTxMethod: () => Promise<PopulatedTransaction>
+  from: tEthereumAddress
+  value?: string
+}
+
+export type transactionType = {
+  value?: string
+  from?: string
+  to?: string
+  nonce?: number
+  gasLimit?: BigNumber
+  gasPrice?: BigNumber
+  data?: string
+  chainId?: number
+}
