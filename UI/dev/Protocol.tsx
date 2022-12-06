b9b0aab424be8b197a20a9bbda19eb5044df1c90
rockgold0911 <rockgold0911@gmail.com>
2022-12-06 11:01:26 +0800

    feat(UI): dev add DevForm

--- UI/dev/Protocol.tsx
+++ UI/dev/Protocol.tsx
@@ -1,10 +1,18 @@
 import type { FC } from 'react'
-import { useMemo } from 'react'
+import { useMemo, Fragment } from 'react'
 import Button from '@mui/material/Button'
 
+import { useDialog } from 'app/hooks/useDialog'
+import { createToastifyPromise } from 'app/utils/promise/toastify'
+import Dialog from 'components/dialog/Dialog'
+
 import { DevCard } from './components/DevCard'
+import { DevForm } from './components/DevForm'
+import { useForm } from 'lib/protocol/dev/api/code-template/form/useForm'
 
 export const Protocol: FC = () => {
+  const codeTemplateDialog = useDialog()
+  const formData = useForm()
   const data = useMemo(() => {
     return [
       {
@@ -13,7 +21,20 @@ export const Protocol: FC = () => {
         action: (
           <Button
             onClick={() => {
-              console.log(fetch('/api/protocol/generate'))
+              createToastifyPromise(fetch('/api/protocol/generate'))
+            }}
+          >
+            执行
+          </Button>
+        ),
+      },
+      {
+        path: '/api/protocol/code-template',
+        description: '生成模板代码',
+        action: (
+          <Button
+            onClick={() => {
+              codeTemplateDialog.open()
             }}
           >
             执行
@@ -21,14 +42,24 @@ export const Protocol: FC = () => {
         ),
       },
     ]
-  }, [])
+  }, [codeTemplateDialog])
 
   return (
-    <DevCard
-      {...{
-        title: 'Protocol',
-        data,
-      }}
-    />
+    <Fragment>
+      <DevCard
+        {...{
+          title: 'Protocol',
+          data,
+        }}
+      />
+      <Dialog
+        {...{
+          ...codeTemplateDialog,
+          title: ' Code Template',
+        }}
+      >
+        <DevForm {...formData} />
+      </Dialog>
+    </Fragment>
   )
 }
---
+++ UI/dev/components/DevForm.tsx
@@ -0,0 +1,39 @@
+import type { FC } from 'react'
+
+import Grid from '@mui/material/Grid'
+import TextField from '@mui/material/TextField'
+import SubmitBotton from 'components/form/SubmitBotton'
+
+type DevFormProps = {
+  formik: any
+  form: any
+}
+
+export const DevForm: FC<DevFormProps> = ({ form, formik }) => {
+  if (!form.fields) return null
+  return (
+    <form onSubmit={form.onSubmit}>
+      <Grid container spacing={2}>
+        {Object.keys(form.fields).map((key) => (
+          <Grid item xs={12} key={key}>
+            <TextField
+              label={key}
+              variant="outlined"
+              name={key}
+              fullWidth
+              value={formik.values[key]}
+              onChange={formik.handleChange}
+              error={formik.touched[key] && Boolean(formik.errors[key])}
+              helperText={formik.touched[key] && formik.errors[key]}
+            />
+          </Grid>
+        ))}
+        <Grid item container xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+          <SubmitBotton size="large" variant="contained" isSubmitting={formik.isSubmitting}>
+            Submit
+          </SubmitBotton>
+        </Grid>
+      </Grid>
+    </form>
+  )
+}
