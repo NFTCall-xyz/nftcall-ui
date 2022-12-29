9579c501bb8803c4d2a82803bf83f06f38d7a229
gordon199404 <gordon199404@outlook.com>
2022-12-29 15:52:04 +0800

    feat(pages): app-buy init

---
+++ app/i18n/locales/en/app-buy.ts
@@ -0,0 +1,16 @@
+const locale = {
+  title: 'Buy NFT Call Options',
+  subTitle: 'Get a right to buy NFTs at a specific price, up until a specified date.',
+  stats: {
+    AccumulativePremium: 'Accumulative Premium',
+    TotalNFTSales: 'Total NFT Sales',
+    TotalDepositedNFTs: 'Total Deposited NFTs',
+    ActiveOptionContracts: 'Active Option Contracts',
+  },
+  callPools: {
+    floorPrice: 'Floor Price',
+    depositedItems: 'Deposited Items',
+  },
+}
+
+export default locale
