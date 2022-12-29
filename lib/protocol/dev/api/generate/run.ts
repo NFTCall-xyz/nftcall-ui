573d690e3957d56bf19461713aa33edbe4f2b965
rockgold0911 <rockgold0911@gmail.com>
2022-12-29 14:52:33 +0800

    feat(lib): update nftcall

--- lib/protocol/dev/api/generate/run.ts
+++ lib/protocol/dev/api/generate/run.ts
@@ -6,13 +6,13 @@ const ROOT_PATH = process.cwd()
 
 const SOURCE_BASE_PATH = path.resolve(ROOT_PATH, 'lib/protocol/src')
 const DEPLOYED_CONTRACTS_PATH = path.resolve(SOURCE_BASE_PATH, 'deployed-contracts.json')
-const DEPLOYED_MARKET_CONTRACTS_PATH = path.resolve(SOURCE_BASE_PATH, 'deployed-market-contracts.json')
+// const DEPLOYED_MARKET_CONTRACTS_PATH = path.resolve(SOURCE_BASE_PATH, 'deployed-market-contracts.json')
 
 const TARGET_BASE_PATH = path.resolve(ROOT_PATH, 'lib/protocol/generate')
 
 export const run = async () => {
   const a = requireFromPath(DEPLOYED_CONTRACTS_PATH)
-  const b = requireFromPath(DEPLOYED_MARKET_CONTRACTS_PATH)
+  // const b = requireFromPath(DEPLOYED_MARKET_CONTRACTS_PATH)
 
   const networks: any = {}
 
@@ -20,26 +20,25 @@ export const run = async () => {
     const v = a[key]
     Object.keys(v).forEach((network) => {
       if (!networks[network]) networks[network] = {}
-      networks[network][key] = v[network].address.toLocaleLowerCase()
+      networks[network][key] = v[network].address
     })
   })
 
   const markets: any = {}
-  Object.keys(b).forEach((key) => {
-    const v = b[key]
-    Object.keys(v).forEach((network) => {
-      if (!networks[network]) networks[network] = {}
-      const v2 = v[network]
-      Object.keys(v2).forEach((marketId) => {
-        if (!markets[network]) markets[network] = {}
-        if (!markets[network][marketId]) markets[network][marketId] = {}
+  const key = 'CallPool'
+  const v = a[key]
+  Object.keys(v).forEach((network) => {
+    if (!networks[network]) networks[network] = {}
+    const v2 = v[network]
+    Object.keys(v2).forEach((marketId) => {
+      if (!markets[network]) markets[network] = {}
+      if (!markets[network][marketId]) markets[network][marketId] = {}
 
-        try {
-          markets[network][marketId][key] = v2[marketId].address.toLocaleLowerCase()
-        } catch (error) {
-          console.log(`${network} ${marketId} ${key}`)
-        }
-      })
+      try {
+        markets[network][marketId][key] = v2[marketId]
+      } catch (error) {
+        console.log(`${network} ${marketId} ${key}`)
+      }
     })
   })
 
