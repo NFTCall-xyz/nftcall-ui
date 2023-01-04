df28060a380661389db2074f8a08bb86edaf13d1
rockgold0911 <rockgold0911@gmail.com>
2023-01-04 10:37:02 +0800

    feat(UI): add positions

--- UI/app/callPool/History/adapter/index.ts
+++ UI/app/callPool/History/adapter/index.ts
@@ -1,14 +1,4 @@
-import { getNumber, getWeiToValueBN, getAddress, safeGet } from 'app/utils/get'
-
-const getERC20SubgraphName = (subgraphName: string) => {
-  const list = subgraphName.split('-')
-  return (
-    safeGet(() => {
-      if (!list[1]) return [list[0], 'erc20']
-      return [list[0], 'erc20', list[1]]
-    }) || []
-  ).join('-')
-}
+import { getNumber, getWeiToValueBN, getAddress } from 'app/utils/get'
 
 type Props = {
   subgraphName: string
@@ -17,16 +7,19 @@ type Props = {
   callPoolAddress: string
 }
 export type SliceState = Array<{
-  id: string
-  type: number
-  amount: BN
-  from: string
-  to: string
-  createTimestamp: number
+  status: string
+  strikePrice: BN
+  premiumToOwner: BN
+  premiumToReserve: BN
+  nftAddress: string
+  userAddress: string
+  tokenId: string
+  endTime: number
+  exerciseTime: number
 }>
 export const request = ({ subgraphName, skip, first, callPoolAddress }: Props) => {
   if (!subgraphName) return Promise.reject({ message: 'network error' })
-  let returnValue: Transaction[] = []
+  let returnValue: Position[] = []
   const promises = []
   promises.push(
     fetch(`https://api.thegraph.com/subgraphs/name/${subgraphName}`, {
@@ -40,19 +33,22 @@ export const request = ({ subgraphName, skip, first, callPoolAddress }: Props) =
       },
       body: JSON.stringify({
         query: `{
-  transactions(
+  positions(
     first: ${first}
     skip: ${skip}
-    where: { vault: ${JSON.stringify(callPoolAddress)} }
+    where: { callPoolAddress: ${JSON.stringify(callPoolAddress)} }
     orderBy: createTimestamp
     orderDirection: desc
   ) {
-    id
-    type
-    amount
-    from
-    to
-    createTimestamp
+    userAddress
+    nftAddress
+    tokenId
+    exerciseTime
+    endTime
+    strikePrice
+    premiumToOwner
+    premiumToReserve
+    status
   }
 }`,
       }),
@@ -61,72 +57,44 @@ export const request = ({ subgraphName, skip, first, callPoolAddress }: Props) =
       credentials: 'omit',
     })
       .then((data) => data.json())
-      .then(({ data: { transactions } }) => transactions as SliceState)
+      .then(({ data: { positions } }) => positions as SliceState)
       .then((data) => {
-        returnValue = returnValue.concat(getTransaction(data))
+        returnValue = returnValue.concat(getPositions(data))
       })
   )
-
-  const erc20SubgraphName = getERC20SubgraphName(subgraphName)
-  if (erc20SubgraphName) {
-    promises.push(
-      fetch(`https://api.thegraph.com/subgraphs/name/${erc20SubgraphName}`, {
-        headers: {
-          accept: '*/*',
-          'accept-language': 'zh-CN,zh;q=0.9',
-          'content-type': 'application/json',
-          'sec-fetch-dest': 'empty',
-          'sec-fetch-mode': 'cors',
-          'sec-fetch-site': 'same-site',
-        },
-        body: JSON.stringify({
-          query: `{
-  transactions(
-    first: ${first}
-    skip: ${skip}
-    where: { vault: ${JSON.stringify(callPoolAddress)} }
-    orderBy: createTimestamp
-    orderDirection: desc
-  ) {
-    id
-    type
-    amount
-    from
-    to
-    createTimestamp
-  }
-}`,
-        }),
-        method: 'POST',
-        mode: 'cors',
-        credentials: 'omit',
-      })
-        .then((data) => data.json())
-        .then(({ data: { transactions } }) => transactions as SliceState)
-        .then((data) => {
-          returnValue = returnValue.concat(getTransaction(data))
-        })
-    )
-  }
   return Promise.all(promises).then(() => returnValue)
 }
 
-export type Transaction = {
-  id: string
-  createTimestamp: number
-  amount: BN
-  from: string
-  to: string
+export type Position = {
+  status: string
+  strikePrice: BN
+  premiumToOwner: BN
+  premiumToReserve: BN
+  nftAddress: string
+  userAddress: string
+  tokenId: string
+  endTime: number
+  exerciseTime: number
 }
-export const getTransaction = (transaction: SliceState) => {
-  if (!transaction) return []
-  const returnValue = transaction.map((t) => {
-    const timestamps = getNumber(t, ['createTimestamp'])
-    const returnValue: Transaction = {
+export const getPositions = (positions: SliceState) => {
+  if (!positions) return []
+  const returnValue = positions.map((t) => {
+    const timestamps = getNumber(t, ['endTime', 'exerciseTime'])
+    const returnValue: Position = {
       ...t,
       ...timestamps,
-      ...getAddress(t, ['from', 'to']),
-      ...getWeiToValueBN(t, ['amount'], 18),
+      ...getAddress(t, ['nftAddress', 'userAddress']),
+      ...getWeiToValueBN(t, ['strikePrice', 'premiumToOwner', 'premiumToReserve'], 18),
+    }
+    if (returnValue.status === 'Unexercised') {
+      const now = Date.now()
+      if (returnValue.endTime < now) {
+        returnValue.status = 'Expired'
+      } else if (returnValue.exerciseTime < now) {
+        returnValue.status = 'Not Exercisible'
+      } else {
+        returnValue.status = 'Exercisible'
+      }
     }
     return returnValue
   })
--- UI/app/callPool/History/renderer.tsx
+++ UI/app/callPool/History/renderer.tsx
@@ -1,5 +1,11 @@
 import TableCell from '@mui/material/TableCell'
 import LinkToAddress from 'components/button/LinkToAddress'
+import { Paragraph } from 'components/Typography'
+
+import { format } from 'date-fns'
+import NumberDisplay from 'lib/math/components/NumberDisplay'
+
+import type { Position } from './adapter'
 
 export type TableCellProps = {
   cellData?: any
@@ -8,15 +14,37 @@ export type TableCellProps = {
   dataKey: string
   isScrolling: boolean
   parent?: any
-  rowData: any
+  rowData: Position
   rowIndex: number
 }
-export const fromCellRenderer = ({ cellData, rowData: { vault, type } }: TableCellProps) => {
-  const defalutName = type === 'Fund Transfer' ? 'CEX' : undefined
-  const name = cellData === vault ? 'Contract' : defalutName
+export const accountCellRenderer = ({ rowData: { userAddress } }: TableCellProps) => {
   return (
     <TableCell align="center" component="div">
-      <LinkToAddress name={name} address={cellData} />
+      <LinkToAddress address={userAddress} />
+    </TableCell>
+  )
+}
+
+export const NFTCellRenderer = ({ rowData: { tokenId, nftAddress } }: TableCellProps) => {
+  return (
+    <TableCell align="center" component="div">
+      <LinkToAddress address={nftAddress} name={tokenId} />
+    </TableCell>
+  )
+}
+
+export const expiryDateCellRenderer = ({ rowData: { endTime } }: TableCellProps) => {
+  return (
+    <TableCell align="center" component="div">
+      <Paragraph>{format(endTime, 'MM/dd/yyyy')}</Paragraph>
+    </TableCell>
+  )
+}
+
+export const premiumCellRenderer = ({ rowData: { premiumToOwner } }: TableCellProps) => {
+  return (
+    <TableCell align="center" component="div">
+      <NumberDisplay value={premiumToOwner} options="number" />
     </TableCell>
   )
 }
