1bfa337921540a445b73d68d8f0d7804998cbf24
gordon199404 <gordon199404@outlook.com>
2023-01-06 16:53:37 +0800

    feat(lib): protocol add NFTOracleService

---
+++ lib/protocol/typechain/nftcall/NFTOracleService.ts
@@ -0,0 +1,29 @@
+import type { providers } from 'ethers'
+import BaseService from '../commons/BaseService'
+import type { tEthereumAddress } from '../commons/types'
+import type { NFTOracle } from './typechain'
+import { NFTOracle__factory } from './typechain'
+
+interface BaseNFTOracleProps {
+  nftOracle: tEthereumAddress
+}
+
+export interface GetAssetsProps extends BaseNFTOracleProps {
+  nftAddresses: tEthereumAddress[]
+}
+
+export class NFTOracleService extends BaseService<NFTOracle> {
+  provider: providers.Provider
+
+  constructor(provider: providers.Provider) {
+    super(provider, NFTOracle__factory)
+    this.provider = provider
+    this.getAssets = this.getAssets.bind(this)
+  }
+
+  public async getAssets(props: GetAssetsProps) {
+    const { nftOracle, nftAddresses } = props
+    const nftOracleContract = this.getContractInstance(nftOracle)
+    return nftOracleContract.getAssets(nftAddresses)
+  }
+}
