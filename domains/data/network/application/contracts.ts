969d7a3612bc1be6643683ccca6f017e4d9aeb24
rockgold0911 <rockgold0911@gmail.com>
2023-01-06 16:53:37 +0800

    feat(lib): protocol add NFTOracleService

--- domains/data/network/application/contracts.ts
+++ domains/data/network/application/contracts.ts
@@ -4,10 +4,12 @@ import type { Provider } from 'lib/protocol/provider/common-static-json-rpc-prov
 import { ERC20Service } from 'lib/protocol/typechain/erc20'
 import { ERC721Service } from 'lib/protocol/typechain/erc721-contract'
 import { CallPoolService } from 'lib/protocol/typechain/nftcall'
+import { NFTOracleService } from 'lib/protocol/typechain/nftcall/NFTOracleService'
 
 export const useContracts = (provider: Provider) => {
   const contracts = useMemo(() => {
     return {
+      nftOracleService: new NFTOracleService(provider),
       callPoolService: new CallPoolService(provider),
       erc20Service: new ERC20Service(provider),
       erc721Service: new ERC721Service(provider),
