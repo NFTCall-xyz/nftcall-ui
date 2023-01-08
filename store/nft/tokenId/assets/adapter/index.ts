ef973a64c2c0f04512f8dd6541fcba384d7dc8dc
gordon199404 <gordon199404@outlook.com>
2023-01-08 22:10:12 +0800

    fix(store): updateTokenIdAssets

--- store/nft/tokenId/assets/adapter/index.ts
+++ store/nft/tokenId/assets/adapter/index.ts
@@ -1,37 +1,54 @@
+import type { AssetsBaseData } from './getAssetsBaseData'
 import { getAssetsBaseData } from './getAssetsBaseData'
 import type { AssetsData } from './getAssetsData'
 import axios from 'axios'
 import type { ChainId } from 'lib/protocol/chain/types'
 import { getOpenSeaUrl } from 'app/constant/openSea'
 import qs from 'qs'
-
+import store from 'store'
+import { safeGet } from 'app/utils/get'
+import { createPromise } from 'app/utils/promise'
+export const getStoreCacheData = () => store.getState().nft.tokenId.assets.data || ([] as undefined)
 export type AssetsProps = {
   chainId: ChainId
-  storeCacheData: AssetsData[]
+  getStoreCacheData: () => AssetsData[]
   nftAddress: string
   tokenIds: string[]
 }
 
-export const assetsRequest = ({ chainId, storeCacheData, nftAddress, tokenIds }: AssetsProps) => {
+export const assetsRequest = ({ chainId, nftAddress, getStoreCacheData, tokenIds }: AssetsProps) => {
+  const storeCacheData = getStoreCacheData()
   const cache = storeCacheData.filter((i) => i.nftAddress === nftAddress)
   const newTokenIds = tokenIds.filter((tokenId) => !cache.find((i) => i.token_id === tokenId))
   if (!newTokenIds.length) return Promise.resolve(storeCacheData)
 
-  return axios
-    .request({
-      method: 'GET',
-      url: getOpenSeaUrl(chainId, '/api/v1/assets'),
-      params: {
-        limit: newTokenIds.length,
-        asset_contract_address: nftAddress,
-        token_ids: newTokenIds,
-      },
-      paramsSerializer: (params) => {
-        return qs.stringify(params, { arrayFormat: 'repeat' })
-      },
-      headers: { accept: 'application/json' },
-    })
-    .then(({ data: { assets } }) => getAssetsBaseData(storeCacheData, nftAddress, assets))
+  const fn = () =>
+    axios
+      .request({
+        method: 'GET',
+        url: getOpenSeaUrl(chainId, '/api/v1/assets'),
+        params: {
+          limit: newTokenIds.length,
+          asset_contract_address: nftAddress,
+          token_ids: newTokenIds,
+        },
+        paramsSerializer: (params) => {
+          return qs.stringify(params, { arrayFormat: 'repeat' })
+        },
+        headers: { accept: 'application/json' },
+      })
+      .then(({ data: { assets } }) => getAssetsBaseData(getStoreCacheData(), nftAddress, assets))
+      .catch((e) => {
+        if (safeGet(() => e.response.status === 429)) {
+          const { promise, reslove } = createPromise<AssetsBaseData[]>()
+          setTimeout(() => reslove(fn()), 1300)
+          return promise
+        } else {
+          return Promise.reject(e)
+        }
+      })
+
+  return fn()
 }
 
 export type AssetsSliceState = Awaited<ReturnType<typeof assetsRequest>>
