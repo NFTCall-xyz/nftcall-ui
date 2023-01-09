6f9aceb5c2b67004eb6433ea64e9db64f2db718c
rockgold0911 <rockgold0911@gmail.com>
2023-01-09 13:31:10 +0800

    fix(emotion): keyframe not supported

--- UI/layouts/home/Main.tsx
+++ UI/layouts/home/Main.tsx
@@ -1,11 +1,7 @@
 import { styled } from '@mui/material/styles'
 import Container from '@mui/material/Container'
 
-import fadeIn from 'app/utils/style/keyframes/fadeIn'
-
-const ROOT: any = styled(Container)`
-  animation: ${fadeIn} 1s ease;
-`
+const ROOT: any = styled(Container)``
 const Main: FCC = ({ children }) => {
   return (
     <ROOT component="main" maxWidth="lg">
