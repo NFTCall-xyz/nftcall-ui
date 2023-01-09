fa65ad70e2ac27074075adcfc1168f60b5fdf831
rockgold0911 <rockgold0911@gmail.com>
2023-01-09 09:31:09 +0800

    feat(domains): network Provider remove getProvider

--- domains/data/network/application/provider.ts
+++ domains/data/network/application/provider.ts
@@ -1,14 +1,9 @@
 import { useWallet } from 'domains'
-import type { AddressData } from 'lib/protocol/market'
-import { getProvider } from 'lib/protocol/provider'
 import type { Provider } from 'lib/protocol/provider/common-static-json-rpc-provider'
 import { useMemo } from 'react'
 
-export const useProvider = (address: AddressData) => {
-  const { ethereum, network } = useWallet()
-  const provider: Provider = useMemo(
-    () => (network && ethereum ? ethereum : getProvider(address.chainId)),
-    [address.chainId, ethereum, network]
-  )
+export const useProvider = () => {
+  const { ethereum } = useWallet()
+  const provider: Provider = useMemo(() => ethereum, [ethereum])
   return provider
 }
