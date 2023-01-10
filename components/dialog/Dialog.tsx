74e2aff0a1732bd5738ba83d3bea4cf5f970c2fe
gordon199404 <gordon199404@outlook.com>
2023-01-10 14:33:44 +0800

    feat(components): Dialog add actions

--- components/dialog/Dialog.tsx
+++ components/dialog/Dialog.tsx
@@ -1,16 +1,18 @@
 import MaterialDialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
 import DialogContent from '@mui/material/DialogContent'
 import DialogTitle from '@mui/material/DialogTitle'
 
 import type { DialogValues } from 'app/hooks/useDialog'
 import { DialogCloseIconButton } from 'components/btn/IconButton'
 
-const Dialog: FCC<DialogValues & { title: string }> = ({ visible, close, title, children }) => {
+const Dialog: FCC<DialogValues & { title: string; actions?: any }> = ({ visible, close, title, children, actions }) => {
   return (
     <MaterialDialog open={visible} onClose={close} maxWidth={false}>
       <DialogCloseIconButton onClick={close} />
       <DialogTitle>{title}</DialogTitle>
       <DialogContent>{children}</DialogContent>
+      {actions && <DialogActions>{actions}</DialogActions>}
     </MaterialDialog>
   )
 }
