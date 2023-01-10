ba7b593e7e4a6a0925b21a6d491a0cf776e6eaaf
rockgold0911 <rockgold0911@gmail.com>
2023-01-10 14:33:20 +0800

    feat(app): constant add callpools

---
+++ app/constant/callPools.ts
@@ -0,0 +1,14 @@
+export const MIN_STRIKE_PRICE_MAP = [
+  { value: 0, label: '0%' },
+  { value: 1, label: '10%' },
+  { value: 2, label: '20%' },
+  { value: 3, label: '30%' },
+  { value: 4, label: '50%' },
+  { value: 5, label: '100%' },
+]
+export const MAX_EXPRIY_TIME_MAP = [
+  { value: 0, label: '3 days' },
+  { value: 1, label: '7 days' },
+  { value: 2, label: '14 days' },
+  { value: 3, label: '28 days' },
+]
