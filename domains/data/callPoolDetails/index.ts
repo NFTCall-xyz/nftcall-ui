570c1d39718426bc201c7019f41f4262ed6f0e24
gordon199404 <gordon199404@outlook.com>
2023-01-14 14:40:28 +0800

    feat(doamins): callPoolDetails add premiums

--- domains/data/callPoolDetails/index.ts
+++ domains/data/callPoolDetails/index.ts
@@ -1,19 +1,40 @@
 import { createContext } from 'app/utils/createContext'
 import { log } from 'app/utils/dev'
+import { safeGet } from 'app/utils/get'
 import { useMemo } from 'react'
+import type { PremiumData } from 'store/callPool/previewOpenCall/adapter/getPreviewOpenCallData'
+import { useCallPoolStateData } from 'store/callPool/useCallPoolStateData'
 import { useCallPools } from '..'
+import type { CallPool } from '../callPools'
 import { useCallPoolId } from './application/callPoolId'
 
+export type CallPoolDetail = CallPool & {
+  premiums: PremiumData[]
+}
+
 const useCallPoolDetailsService = () => {
+  const storeData = useCallPoolStateData()
   const { callPools } = useCallPools()
   const callPoolId = useCallPoolId()
   const callPool = useMemo(() => {
-    const returnValue = callPoolId.value
-      ? callPools.find((callPool) => callPool.address.CallPool.toLowerCase() === callPoolId.value.toLowerCase())
-      : undefined
+    let returnValue: CallPoolDetail = {} as any
+    if (!callPoolId.value) return undefined
+    const callPool = callPools.find(
+      (callPool) => callPool.address.CallPool.toLowerCase() === callPoolId.value.toLowerCase()
+    )
+    if (!callPool) return undefined
+    const premiums =
+      safeGet(() => storeData.previewOpenCall.find((i) => i.callPool === callPool.address.CallPool).premiums) ||
+      ([] as undefined)
+
+    returnValue = {
+      ...callPool,
+      premiums,
+    }
     log('[CallPoolDetailsService]', returnValue)
     return returnValue
-  }, [callPoolId.value, callPools])
+  }, [callPoolId.value, callPools, storeData.previewOpenCall])
+
   return {
     callPool,
     callPoolId,
