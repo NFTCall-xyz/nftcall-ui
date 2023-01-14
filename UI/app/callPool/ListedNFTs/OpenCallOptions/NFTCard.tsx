d46575fe0e9e80d235494dbaa54bca91563bec57
gordon199404 <gordon199404@outlook.com>
2023-01-14 15:40:43 +0800

    style: OpenCallOptions NFTCard

--- UI/app/callPool/ListedNFTs/OpenCallOptions/NFTCard.tsx
+++ UI/app/callPool/ListedNFTs/OpenCallOptions/NFTCard.tsx
@@ -3,15 +3,13 @@ import { useMemo } from 'react'
 import { useTranslation } from 'next-i18next'
 import { styled } from '@mui/material/styles'
 import Card from '@mui/material/Card'
-import Stack from '@mui/material/Stack'
 import type { ListedNFT } from '../NFTCard'
 import DeleteIcon from '@mui/icons-material/Delete'
 import Grid from '@mui/material/Grid'
 import IconButton from '@mui/material/IconButton'
-import { H3, Paragraph } from 'components/Typography'
+import { Paragraph } from 'components/Typography'
 import { useNFTAssetsData } from 'domains/data/nft/hooks/useNFTAssetsData'
 import NFTIcon from 'domains/data/nft/components/NFTIcon'
-import { safeGet } from 'app/utils/get'
 
 export type NFTCardProps = {
   tokenId: string
@@ -32,14 +30,18 @@ const NFTCard: FC<NFTCardProps> = ({ data, tokenId, onCheckChange }) => {
   return (
     <ROOT>
       <Grid container spacing={2}>
-        <Grid item xs={2}>
+        <Grid item xs={2.5}>
           <NFTIcon nftAssetsData={nftAssetsData} />
         </Grid>
-        <Grid item xs={8}>
-          <Stack spacing={1}>
-            <H3>{safeGet(() => nftAssetsData.contractName) || ''}</H3>
-            <Paragraph>{title}</Paragraph>
-          </Stack>
+        <Grid
+          item
+          xs={7.5}
+          sx={{
+            alignItems: 'center',
+            display: 'flex',
+          }}
+        >
+          <Paragraph>{title}</Paragraph>
         </Grid>
         <Grid item xs={1}>
           <IconButton
