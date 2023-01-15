75e22a258168c278e81f093ca7abb70d92fc7099
gordon199404 <gordon199404@outlook.com>
2023-01-15 10:40:26 +0800

    feat(domians): nft add NFTActions

--- domains/data/nft/types.ts
+++ domains/data/nft/types.ts
@@ -3,10 +3,15 @@ export type BaseNFT = {
   tokenId: string
   nftAddress: string
 }
+export type NFTActions = Partial<{
+  setStatus: (status: NFTStatus) => void
+  setLoading: (loading: boolean) => void
+}>
 export type NFT = BaseNFT &
   Partial<{
     callPoolAddress: string
     status: NFTStatus
     minStrikePrice: number
     maxExpriyTime: number
+    actions: NFTActions
   }>
