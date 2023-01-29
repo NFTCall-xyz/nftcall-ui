9e50e49719afd430486bfd789ada2f6b6e53bcaf
gordon199404 <gordon199404@outlook.com>
2023-01-29 22:43:23 +0800

    fix(UI): claim remove useUpdateNFTAssets

--- UI/app/claim/Claims/index.tsx
+++ UI/app/claim/Claims/index.tsx
@@ -1,11 +1,9 @@
 import BasicTable from 'components/table/BasicTable'
-import { useUpdateNFTAssets } from 'domains/data/nft/hooks/useUpdateNFTAssets'
 
 import { useTable } from './useTable'
 
 const Claims = () => {
   const table = useTable()
-  useUpdateNFTAssets(table.data)
 
   return <BasicTable {...table} />
 }
