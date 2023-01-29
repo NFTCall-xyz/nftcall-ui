28b0fcc92ae335ac751d49d121fdf53ebd0b46c7
gordon199404 <gordon199404@outlook.com>
2023-01-29 16:53:48 +0800

    feat(UI): ListedNFTs add invalidated

--- UI/app/callPool/ListedNFTs/OpenCallOptions/NFTCard.tsx
+++ UI/app/callPool/ListedNFTs/OpenCallOptions/NFTCard.tsx
@@ -5,6 +5,7 @@ import Card from '@mui/material/Card'
 import type { ListedNFT } from '../NFTCard'
 import DeleteIcon from '@mui/icons-material/Delete'
 import IconButton from '@mui/material/IconButton'
+import Box from '@mui/material/Box'
 import { Paragraph } from 'components/Typography'
 import { useNFTAssetsData } from 'domains/data/nft/hooks/useNFTAssetsData'
 import NFTIcon from 'domains/data/nft/components/NFTIcon'
@@ -26,11 +27,18 @@ const NFTCard: FC<NFTCardProps> = ({ data, tokenId, onCheckChange }) => {
   const { nftAssetsData } = useNFTAssetsData(listedNFT)
   const title = `#${tokenId}`
 
+  const invalidated = !listedNFT
+
   return (
     <ROOT>
       <FlexBetween>
         <Stack direction="row" alignItems="center" spacing={2}>
-          <NFTIcon nftAssetsData={nftAssetsData} sx={{ width: 40, height: 40 }} />
+          {invalidated ? (
+            <Box sx={{ width: 40, height: 40 }}>invalidated</Box>
+          ) : (
+            <NFTIcon nftAssetsData={nftAssetsData} sx={{ width: 40, height: 40 }} />
+          )}
+
           <Paragraph>{title}</Paragraph>
         </Stack>
         <IconButton
