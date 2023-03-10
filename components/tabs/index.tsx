e57df3aefa4837fdaf3fd3f2a3e68e156736295c
gordon199404 <gordon199404@outlook.com>
2023-03-10 15:28:30 +0800

    style(components): tabs fix

--- components/tabs/index.tsx
+++ components/tabs/index.tsx
@@ -64,7 +64,7 @@ const Tabs: FCC<TabsProps> = ({ tabs, Header, sx }) => {
   return (
     <TabContext value={value}>
       {Header ? <Header>{tabList}</Header> : tabList}
-      <Grid container pt={4}>
+      <Grid container marginTop={2}>
         <Grid item xs={12}>
           {tabs.map(({ title, children }, index) => {
             return (
