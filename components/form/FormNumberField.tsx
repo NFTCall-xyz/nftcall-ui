47fd8e8db5e16f94625284c5d65c179c763e6063
gordon199404 <gordon199404@outlook.com>
2023-03-10 15:28:30 +0800

    feat(components): FormNumberFieldField add disabled

--- components/form/FormNumberField.tsx
+++ components/form/FormNumberField.tsx
@@ -5,10 +5,12 @@ import { NumberInput } from 'lib/math/components/NumberInput'
 type FormNumberFieldFieldProps = {
   formik: any
   fieldKey: string
+  disabled?: boolean
 }
 const FormNumberFieldField: FCC<FormNumberFieldFieldProps> = ({
   formik: { values, errors, handleBlur, handleChange, touched },
   fieldKey,
+  disabled,
   children,
 }) => {
   return (
@@ -16,6 +18,7 @@ const FormNumberFieldField: FCC<FormNumberFieldFieldProps> = ({
       name={fieldKey}
       onBlur={handleBlur}
       onChange={handleChange}
+      disabled={disabled}
       value={values[fieldKey]}
       helperText={safeGet(() => touched[fieldKey] && errors[fieldKey])}
       error={!!safeGet(() => touched[fieldKey] && errors[fieldKey])}
