15d0c2fb5542b701ce35745475c1e35cc6b945ed
gordon199404 <gordon199404@outlook.com>
2023-03-13 18:16:48 +0800

    feat(components): Dialog add Scrollbar

--- components/dialog/Dialog.tsx
+++ components/dialog/Dialog.tsx
@@ -1,4 +1,7 @@
+import { useMemo } from 'react'
+
 import type { Breakpoint } from '@mui/material'
+import Box from '@mui/material/Box'
 import MaterialDialog from '@mui/material/Dialog'
 import DialogActions from '@mui/material/DialogActions'
 import DialogContent from '@mui/material/DialogContent'
@@ -6,17 +9,41 @@ import DialogTitle from '@mui/material/DialogTitle'
 
 import type { DialogValues } from 'app/hooks/useDialog'
 
+import Scrollbar from 'components/ScrollBar'
 import { DialogCloseIconButton } from 'components/btn/IconButton'
 
 const Dialog: FCC<
-  DialogValues & { title: string; actions?: any; maxWidth?: false | Breakpoint; fullScreen?: boolean, fullWidth?: boolean }
+  DialogValues & {
+    title: string
+    actions?: any
+    maxWidth?: false | Breakpoint
+    fullScreen?: boolean
+    fullWidth?: boolean
+  }
 > = ({ visible, close, title, children, actions, maxWidth, fullScreen, fullWidth }) => {
+  const maxWidthMemo = useMemo(() => maxWidth || (fullWidth ? 'lg' : false), [fullWidth, maxWidth])
   return (
-    <MaterialDialog open={visible} onClose={close} maxWidth={maxWidth || false} fullScreen={fullScreen} fullWidth={fullWidth}>
-      <DialogCloseIconButton onClick={close} />
-      <DialogTitle>{title}</DialogTitle>
-      <DialogContent>{children}</DialogContent>
-      {actions && <DialogActions sx={{ padding: 3 }}>{actions}</DialogActions>}
+    <MaterialDialog
+      open={visible}
+      onClose={close}
+      maxWidth={maxWidthMemo}
+      fullScreen={fullScreen}
+      fullWidth={fullWidth}
+    >
+      <Box sx={{ flexGrow: 0, flexShrink: 0, flexBasis: 'auto' }}>
+        <DialogCloseIconButton onClick={close} />
+        <DialogTitle>{title}</DialogTitle>
+      </Box>
+
+      <Box sx={{ flexGrow: 1, flexShrink: 1, flexBasis: 'auto', overflow: 'hidden' }}>
+        <Scrollbar sx={{ maxHeight: '100%' }}>
+          <DialogContent>{children}</DialogContent>
+        </Scrollbar>
+      </Box>
+
+      {actions && (
+        <DialogActions sx={{ padding: 3, flexGrow: 0, flexShrink: 0, flexBasis: 'auto' }}>{actions}</DialogActions>
+      )}
     </MaterialDialog>
   )
 }
