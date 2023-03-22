b74540eb93345be6ca59f7830354363de0980033
CryptoLory <>
2023-03-22 17:10:18 +0800

    style: chain button icon

--- lib/protocol/components/wallet/ChainIcon/index.tsx
+++ lib/protocol/components/wallet/ChainIcon/index.tsx
@@ -1,7 +1,7 @@
 import type { FC } from 'react'
 
 import CircleIcon from '@mui/icons-material/FiberManualRecordRounded'
-
+import TokenIcon from 'lib/protocol/components/TokenIcon'
 interface ChainIconProps {
   chainName: string
 }
@@ -11,6 +11,8 @@ export const ChainIcon: FC<ChainIconProps> = ({ chainName }) => {
     switch (name) {
       case 'Goerli':
         return <CircleIcon fontSize="small" sx={{ color: '#f6c343' }} />
+      case 'Ethereum':
+        return <TokenIcon symbol={'ETH'} sx={{ width: 16, height: 16 }} />
       default:
         return <CircleIcon fontSize="small" sx={{ color: 'grey' }} />
     }
