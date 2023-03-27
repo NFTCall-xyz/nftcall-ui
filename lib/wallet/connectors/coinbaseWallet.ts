d8f26a2ba87e2c4e57a26d57216bcbb87478ee8d
gordon199404 <gordon199404@outlook.com>
2023-03-27 09:33:49 +0800

    feat(lib): wallet update connectors

---
+++ lib/wallet/connectors/coinbaseWallet.ts
@@ -0,0 +1,17 @@
+import { CoinbaseWallet } from '@web3-react/coinbase-wallet'
+import { initializeConnector } from '@web3-react/core'
+
+import { ChainId } from 'lib/protocol/chain/types'
+
+import { getChainInformationByChainId } from '../constant/chains'
+
+export const [coinbaseWallet, hooks] = initializeConnector<CoinbaseWallet>(
+  (actions) =>
+    new CoinbaseWallet({
+      actions,
+      options: {
+        url: getChainInformationByChainId(ChainId.ethereum).publicJsonRPCUrl[0],
+        appName: 'NFTCall',
+      },
+    })
+)
---
+++ lib/wallet/connectors/walletConnectV2.ts
@@ -0,0 +1,16 @@
+import { initializeConnector } from '@web3-react/core'
+import { WalletConnect as WalletConnectV2 } from '@web3-react/walletconnect-v2'
+
+import { CHAIN_IDS } from '../constant/chains'
+
+export const [walletConnectV2, hooks] = initializeConnector<WalletConnectV2>(
+  (actions) =>
+    new WalletConnectV2({
+      actions,
+      options: {
+        projectId: process.env.walletConnectProjectId,
+        chains: CHAIN_IDS,
+        showQrModal: true,
+      },
+    })
+)
