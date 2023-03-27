372540aa3e5864358eafcf69662757c30dfd460c
gordon199404 <gordon199404@outlook.com>
2023-03-27 16:32:49 +0800

    feat(utils): createToastifyPromise add options

--- app/utils/promise/toastify.ts
+++ app/utils/promise/toastify.ts
@@ -1,23 +1,33 @@
 import type { FormikHelpers } from 'formik'
 
+import type { ToastContent, ToastPosition } from 'lib/toastify'
 import { toast } from 'lib/toastify'
 
 import { catchError } from '../catch/error'
 import { safeGet } from '../get'
 
 type Options = {
+  pendingContent?: ToastContent
+  resolveContent?: ToastContent
+  rejectedContent?: ToastContent
+  position?: ToastPosition
   formikHelpers?: FormikHelpers<any>
   serializeError?: (e: any) => { message: string; errors: any }
 }
 export function createToastifyPromise<T = void>(p: Promise<T>, options: Options = {}) {
-  const { formikHelpers } = options
-  const toastId = toast.loading('Transaction is pending', {
-    position: toast.POSITION.BOTTOM_RIGHT,
+  const { position, formikHelpers, pendingContent, resolveContent, rejectedContent } = options
+  const toastId = toast.loading(pendingContent || 'Transaction is pending', {
+    position: position || toast.POSITION.BOTTOM_RIGHT,
   })
 
   return p
     .then((data?: T) => {
-      toast.update(toastId, { render: 'Transaction success 👌', type: 'success', isLoading: false, autoClose: 5000 })
+      toast.update(toastId, {
+        render: resolveContent || 'Transaction success 👌',
+        type: 'success',
+        isLoading: false,
+        autoClose: 5000,
+      })
       return data
     })
     .catch((e) => {
@@ -25,7 +35,7 @@ export function createToastifyPromise<T = void>(p: Promise<T>, options: Options 
       const serializeError = safeGet(() => options.serializeError(e)) || ({} as undefined)
       formikHelpers?.setErrors(serializeError.errors)
       toast.update(toastId, {
-        render: serializeError.message || 'Transaction rejected 🤯',
+        render: rejectedContent || serializeError.message || 'Transaction rejected 🤯',
         type: 'error',
         isLoading: false,
         autoClose: 5000,
